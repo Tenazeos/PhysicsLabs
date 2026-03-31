97e600731355040b44a1ee82b6f9803788528fc7
KruASe76 <kruanse@ya.ru>
2026-04-01 02:42:47 +0300

    feat: add animation, finish frontend & description

--- labs/magnetic_trap/page.py
+++ labs/magnetic_trap/page.py
@@ -1,9 +1,9 @@
 import numpy as np
-import plotly.express as px
 import streamlit as st
 
 from .calculations import MovementProcessor
 from .models import MagneticTrap, Particle, SegmentedRing, State
+from .visualization.animation import render_particle_animation
 
 TIME_DELTA = 0.01
 FRAME_COUNT = 1000
@@ -43,6 +43,8 @@ def page() -> None:
             * SI_TO_SGSM_COEF
         )
 
+        st.space()
+
         particle_mass = (
             st.slider(
                 "Particle mass (mg)",
@@ -63,45 +65,70 @@ def page() -> None:
             )
             * SI_TO_SGSM_COEF
         )
-        particle_launch_angle = st.slider(
-            "Particle launch angle (deg)",
-            min_value=-90.0,
-            max_value=90.0,
-            value=-30.0,
-            step=0.1,
+        particle_start_offset = st.slider(
+            "Particle start offset [horizontal] (cm)",
+            min_value=0.0,
+            max_value=10.0,
+            value=3.0,
+            step=0.01,
+        )
+        particle_velocity = st.slider(
+            "Particle velocity (cm/s)",
+            min_value=-5.0,
+            max_value=5.0,
+            value=1.5,
+            step=0.01,
+        )
+        particle_launch_pitch = np.radians(
+            st.slider(
+                "Particle launch vertical angle [pitch] (deg)",
+                min_value=-90.0,
+                max_value=90.0,
+                value=30.0,
+                step=0.1,
+            )
+        )
+        particle_launch_yaw = np.radians(
+            st.slider(
+                "Particle launch horizontal angle [yaw] (deg)",
+                min_value=-90.0,
+                max_value=90.0,
+                value=00.0,
+                step=0.1,
+            )
         )
 
-    center_between_rings = np.array([0, 0, ring_distance / 2])
-    particle_velocity_norm = 1
-    particle_velocity = np.array(
+    center_between_rings = np.array([0, particle_start_offset, ring_distance / 2])
+    initial_particle_velocity_vector = np.array(
         [
-            -particle_velocity_norm * np.cos(particle_launch_angle) / np.sqrt(2),
-            particle_velocity_norm * np.cos(particle_launch_angle) / np.sqrt(2),
-            particle_velocity_norm * np.sin(particle_launch_angle),
+            -particle_velocity * np.cos(particle_launch_pitch) * np.sin(particle_launch_yaw),
+            -particle_velocity * np.cos(particle_launch_pitch) * np.cos(particle_launch_yaw),
+            particle_velocity * np.sin(particle_launch_pitch),
         ]
     )
 
-    movement = MovementProcessor(
-        trap=MagneticTrap(
-            upper_ring=SegmentedRing(
-                radius=ring_radius,
-                z_cord=ring_distance,
-                segment_count=RING_SEGMENT_COUNT,
-            ),
-            lower_ring=SegmentedRing(
-                radius=ring_radius,
-                z_cord=0,
-                segment_count=RING_SEGMENT_COUNT,
-            ),
-            current=ring_current,
+    trap = MagneticTrap(
+        upper_ring=SegmentedRing(
+            radius=ring_radius,
+            z_cord=ring_distance,
+            segment_count=RING_SEGMENT_COUNT,
         ),
+        lower_ring=SegmentedRing(
+            radius=ring_radius,
+            z_cord=0,
+            segment_count=RING_SEGMENT_COUNT,
+        ),
+        current=ring_current,
+    )
+    movement = MovementProcessor(
+        trap=trap,
         particle=Particle(
             mass=particle_mass,
             charge=particle_charge,
         ),
         start_state=State(
             position=center_between_rings,
-            velocity=particle_velocity,
+            velocity=initial_particle_velocity_vector,
         ),
     )
 
@@ -110,26 +137,14 @@ def page() -> None:
         movement.process(TIME_DELTA)
         states.append(movement.state)
 
-    fig = px.scatter_3d(
-        [
-            {
-                "x": state.position[0],
-                "y": state.position[1],
-                "z": state.position[2],
-            }
-            for state in states
-        ],
-        x="x",
-        y="y",
-        z="z",
-        size_max=10,
-        opacity=0.7,
+    st.plotly_chart(
+        render_particle_animation(
+            trap=trap, initial_velocity=initial_particle_velocity_vector, states=states
+        )
     )
-
-    fig.update_layout(
-        scene={"xaxis_title": "X", "yaxis_title": "Y", "zaxis_title": "Z"},
-        width=800,
-        height=600,
+    st.markdown(
+        "<p style='text-align: center; color: #888888;'>"
+        "Sadly, the point of view resets when clicking <b>Play</b> button."
+        "</p>",
+        unsafe_allow_html=True,
     )
-
-    st.plotly_chart(fig, width="stretch")
